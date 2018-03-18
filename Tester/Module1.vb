02840192798c492994e4dfca11349eb59444d201
Matthew Wardle <mattydwardle@gmail.com>
2018-03-18 21:48:51 +0000

    Almost there Moved everything to the main sub (to make it easeir to copy the code across) Also I'm so close to being able to check if the brute force will work corretly but I'm having problems with a global variable

--- Tester/Module1.vb
+++ Tester/Module1.vb
@@ -8,9 +8,17 @@ Imports Newtonsoft.Json
 Imports Newtonsoft.Json.Linq
 Imports System.Text.RegularExpressions
 
+Public Class Shorter
+    Public distance As Integer
+    Public duration As Integer
+    Public nodes As List(Of Integer)
+End Class
+
 Module Module1
+    Public shortest As Shorter
     Sub Main()
-        Dim latLngKey As String = "AIzaSyA3A7tDgpFISYEY3B5qYdXm9StRa0pJkcA"
+        'shortest.distance = 2147483646
+        'Dim latLngKey As String = "AIzaSyA3A7tDgpFISYEY3B5qYdXm9StRa0pJkcA"
         Dim node_list As New List(Of String)
         Dim cont As Boolean = True
 
@@ -39,6 +47,10 @@ Module Module1
         List_print(node_list, False)
 
         Faith_Permute(node_list.Count, node_list, cont)
+
+        For t As Integer = 0 To shortest.nodes.Count - 1
+            '    Console.Write(Array(t) & ": " & Nodes.Item(Array(t)) & ", ")
+        Next
         Console.ReadLine()
     End Sub
 
@@ -66,7 +78,7 @@ Module Module1
     End Function
 
 
-    Function Waypointing(ByRef array() As Integer, ByVal length As String, ByRef nodes As List(Of String))
+    Function Waypointing(ByRef array() As Integer, ByVal length As String, ByRef nodes As List(Of String)) As Integer()
         'Builds request string
         Dim waypointkey As String = "AIzaSyBqN-1pDwR8taEDQESDP5mnJjiJkIXmv-w"
         Dim request_url As String = "https://maps.googleapis.com/maps/api/directions/json?origin="
@@ -127,30 +139,126 @@ Module Module1
 
         Console.WriteLine("Distance: " & distance & " (m)")
         Console.WriteLine("Duration: " & duration & " (s)" & vbTab & Math.Floor(duration / 3600) & " hours  " & Math.Round((duration Mod 3600) / 60) & " minutes")
-        Return distance And duration
+        Dim passed(1) As Integer
+        passed(0) = distance
+        passed(1) = duration
+        Return passed
     End Function
 
 
-    Function FindLat(ByVal destination As String, ByVal key As String)
-        'https://www.aspsnippets.com/Articles/Find-Co-ordinates-Latitude-And-Longitude-of-an-Address-Location-using-Google-Geocoding-API-in-ASPNet-using-C-And-VBNet.aspx
-        Dim url2 As String = "https://maps.googleapis.com/maps/api/geocode/json?address=" & destination & "&key=" & key
+    'Function FindLat(ByVal destination As String, ByVal key As String)
+    '    'https://www.aspsnippets.com/Articles/Find-Co-ordinates-Latitude-And-Longitude-of-an-Address-Location-using-Google-Geocoding-API-in-ASPNet-using-C-And-VBNet.aspx
+    '    Dim url2 As String = "https://maps.googleapis.com/maps/api/geocode/json?address=" & destination & "&key=" & key
 
-        'links to google's API stuff (really really useful)  https://developers.google.com/maps/documentation/geocoding/start?hl=en_US
-        'https://developers.google.com/maps/documentation/directions/intro#Waypoints
-        'https://developers.google.com/maps/documentation/directions/?hl=en_US
+    '    'links to google's API stuff (really really useful)  https://developers.google.com/maps/documentation/geocoding/start?hl=en_US
+    '    'https://developers.google.com/maps/documentation/directions/intro#Waypoints
+    '    'https://developers.google.com/maps/documentation/directions/?hl=en_US
 
-        Dim request As WebRequest = WebRequest.Create(url2)
-        Using response As WebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
-            Using reader As New StreamReader(response.GetResponseStream(), Encoding.UTF8)
-                Dim dsResult As New DataSet()
-                dsResult.ReadXml(reader)
-                Dim dtCoordinates As New DataTable()
-                dtCoordinates.Columns.AddRange(New DataColumn(3) {New DataColumn("Id", GetType(Integer)), New DataColumn("Address", GetType(String)), New DataColumn("Latitude", GetType(String)), New DataColumn("Longitude", GetType(String))})
-                Dim geometry_id As String = dsResult.Tables("geometry").[Select]("result_id = " + ("result_id").ToString())(0)("geometry_id").ToString()
-                Dim location As DataRow = dsResult.Tables("location").[Select](Convert.ToString("geometry_id = ") & geometry_id)(0)
-                'dtCoordinates.Rows.Add(row("result_id"), row("formatted_address"), location("lat"), location("lng"))
-                Return location("Lat")
-            End Using
-        End Using
-    End Function
+    '    Dim request As WebRequest = WebRequest.Create(url2)
+    '    Using response As WebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
+    '        Using reader As New StreamReader(response.GetResponseStream(), Encoding.UTF8)
+    '            Dim dsResult As New DataSet()
+    '            dsResult.ReadXml(reader)
+    '            Dim dtCoordinates As New DataTable()
+    '            dtCoordinates.Columns.AddRange(New DataColumn(3) {New DataColumn("Id", GetType(Integer)), New DataColumn("Address", GetType(String)), New DataColumn("Latitude", GetType(String)), New DataColumn("Longitude", GetType(String))})
+    '            Dim geometry_id As String = dsResult.Tables("geometry").[Select]("result_id = " + ("result_id").ToString())(0)("geometry_id").ToString()
+    '            Dim location As DataRow = dsResult.Tables("location").[Select](Convert.ToString("geometry_id = ") & geometry_id)(0)
+    '            'dtCoordinates.Rows.Add(row("result_id"), row("formatted_address"), location("lat"), location("lng"))
+    '            Return location("Lat")
+    '        End Using
+    '    End Using
+    'End Function
+
+
+    Public Sub Faith_Permute(ByVal length As Integer, ByRef nodes As List(Of String), ByVal End_dest As Boolean)
+        'Creates the variables to be used in the algorithm
+        Dim P(length - 1) As Integer
+        Dim swapper As Integer
+        Dim initial_comp As Integer
+        Dim rearrange As Integer
+        Dim asc_swapper As Integer
+        Dim count As Long
+        Dim Last As Boolean
+
+        'Fills the array with pointers that can be sorted
+        For i = 0 To length - 1
+            P(i) = i
+        Next
+        'Checks if there is a set end point that should not be shuffled
+        If End_dest = True Then
+            length -= 1
+        End If
+
+        count = 0
+        Last = False
+        Dim watch As Stopwatch = Stopwatch.StartNew()   'records the time it takes for all the permutations to be calculated (temp)
+
+        Do While Not Last
+            'outputs the pointers and destinations in order
+            Sort_array(P, length, nodes, End_dest)
+            count = count + 1
+            Last = True
+            initial_comp = length - 2
+            'finds the largest pointer out of place (from the back)
+            Do While initial_comp > 0
+                If P(initial_comp) < P(initial_comp + 1) Then
+                    Last = False
+                    Exit Do
+                End If
+                initial_comp = initial_comp - 1
+            Loop
+
+            rearrange = initial_comp + 1
+            asc_swapper = length - 1
+            'rearranges all the pointers behind the out of place pointer into ascending numerical order
+            While rearrange < asc_swapper
+                ' Swap p(j) and p(k)
+                swapper = P(rearrange)
+                P(rearrange) = P(asc_swapper)
+                P(asc_swapper) = swapper
+                rearrange += 1
+                asc_swapper -= 1
+            End While
+
+            rearrange = length - 1
+            'finds the pointer to be swapped with the out of place pointer
+            While P(rearrange) > P(initial_comp)
+                rearrange = rearrange - 1
+            End While
+
+            rearrange = rearrange + 1
+
+            'makes the swap to place the large number in front
+            swapper = P(initial_comp)
+            P(initial_comp) = P(rearrange)
+            P(rearrange) = swapper
+        Loop
+        watch.Stop()
+        Console.WriteLine("Number of permutations: " & count & vbTab & "That was: " & watch.Elapsed.TotalMilliseconds & "ms, " & watch.ElapsedTicks & " ticks")
+
+    End Sub
+
+    Public Sub Sort_array(ByRef array() As Integer, ByVal length As Integer, ByRef nodes As List(Of String), ByVal end_dest As Boolean)
+        shortest.distance = 2147483646
+        If end_dest = True Then 'if there was an end destination being used then the length must be increased so that it is printed
+            length += 1
+        End If
+
+        For t As Integer = 0 To length - 1
+            Console.Write(array(t) & ": " & nodes.Item(array(t)) & ", ")
+        Next
+
+        Dim test() As Integer = Waypointing(array, length, nodes)   'sends this to the function that should return the length/duration of the route
+
+        Dim temp_dist As Integer = test(0)  'takes the returned data and saves it locally
+        Dim temp_dura As Integer = test(1)
+
+        If temp_dist < shortest.distance Then   'compares the existing shortest route with the current one
+            shortest.distance = temp_dist
+            shortest.distance = temp_dura
+            For i As Integer = 0 To array.Length
+                shortest.nodes(i) = array(i)
+            Next
+        End If
+    End Sub
 End Module
